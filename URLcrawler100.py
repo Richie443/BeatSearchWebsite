f1852d12f30032521dcd09b26a0207eae69ff5ce
paul52841018 <paul52841018@gmail.com>
2020-09-27 15:32:24 +0800

    Update : Now can using thread Downloading Url to Mp3

--- URLcrawler100.py
+++ URLcrawler100.py
@@ -7,24 +7,57 @@ Created on Sat Sep 26 19:57:46 2020
 
 import bs4 
 from selenium import webdriver 
-from URLtoMp3 import TransURLToMp3,MoveFileToFolder
+#from URLtoMp3 import TransURLToMp3,MoveFileToFolder
 import time
 from selenium.webdriver.common.keys import Keys
+import youtube_dl
+import os
+import threading
 
 youtubeLinks={}
+#URL to Mp3 轉檔 (存在程式同層)
+def TransURLToMp3(URL,ID):
+    print("Download Link : ","%s"%URL)
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(URL, download=False)
+        video_title = info_dict.get('title', None)
+    
+    #path = f'.\\{video_title}.mp3'
+    path = f'.\\{ID}.mp3'
+    ydl_opts.update({'outtmpl':path})
+    
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([URL])
+        print("Success")
+
+def MoveFileToFolder(folderName):
+    #將程式同層的mp3 存進MP3_Files
+    if not os.path.exists(folderName):
+        os.mkdir(folderName)
+    os.system('move .\\*.mp3 .\\{folderName}\\')
+    print("finish")
+
 def findURL(soup,idCount):
     hrefs = soup.find_all("a", class_="yt-simple-endpoint style-scope ytd-video-renderer")
     for a in hrefs:
         idCount+=1
         youtubeLinks[idCount]=("https://www.youtube.com/"+a['href'])
-        print(idCount,youtubeLinks[idCount])
+        #print(idCount,youtubeLinks[idCount])
         if(youtubeLinks[idCount] in youtubeLinks):
             print("Repeat",youtubeLinks[id])
     return idCount
         
         
 def main():
-    string = "pop+beat";
+    folderName=string = "old+school+type+beat"
     
     url = "https://www.youtube.com/results?search_query=" + string
     
@@ -43,12 +76,18 @@ def main():
         source = browser.page_source
         soup = bs4.BeautifulSoup(source,'html.parser')
         urlCount=findURL(soup,urlCount)
-        print(urlCount) 
+        #print(urlCount) 
 
     print("End for Search，Prepare to Download and Trans")
     for key,value in youtubeLinks.items():
-        TransURLToMp3(value,key)
+        
+        try:
+            #print(key,value)
+            print(key," Now prepare Download")
+            threading.Thread(target=TransURLToMp3(value,key)).start()
+        except:
+            print("Key : ",key," has some Problem Error !! ")
     print("Finished Download ，Excute MoveFile")
-    MoveFileToFolder()
+    MoveFileToFolder(folderName)
         
 main()
